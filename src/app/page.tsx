5dca9b5a5a72c41a85e98776a22e39c7e6c491c3
David Graham White <7415754+dave-white@users.noreply.github.com>
2025-10-28 14:02:10 -0500

    Add a gallery of tiles linking to the individual recipes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,47 @@
+'use client';
+
 import Link from 'next/link';
+import Image from 'next/image';
+import { Card, CardHeader, Typography } from '@material-tailwind/react';
+
+const RECIPE_DATA = [
+	{
+		"id": "skillet-cookie",
+		"thumbnail": "/img/cookie-cake.jpg",
+		"name": "Skillet Cookie"
+	},
+	{
+		"id": "chicken-marengo",
+		"thumbnail": "/img/chicken-marengo.jpg",
+		"name": "Chicken Marengo"
+	},
+	{
+		"id": "honey-teriyaki-salmon",
+		"thumbnail": "/img/honey-teriyaki-salmon.jpg",
+		"name": "Honey Teriyaki Salmon"
+	},
+]
+
+const recipeCards = RECIPE_DATA.map(recipe => (
+	<Link key={recipe.id} href={{pathname: "recipes", query: {recipeId: recipe.id}}}>
+		<Card>
+			<CardHeader floated={false} className='h-50'>
+				<Image src={recipe.thumbnail} alt={recipe.name} width={250} height={250} />
+			</CardHeader>
+			<Typography variant='h4' className='mb-2'>
+				{recipe.name}
+			</Typography>
+		</Card>
+	</Link>
+))
 
 export default function Home() {
 	return (
 		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-				<Link href="recipes/skillet-cookie">Skillet cookie</Link>
+				<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+					{recipeCards}
+				</div>
 			</main>
 		</div>
 	);
