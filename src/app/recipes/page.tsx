51732f0f357eb88e40ee1395a1921d7edd1ca02a
David Graham White <7415754+dave-white@users.noreply.github.com>
2025-10-28 22:42:58 -0500

    Fetch recipe data from PostgreSQL database.

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,18 +1,37 @@
-import fsPromises from 'fs/promises';
-import path from 'path';
+// import fsPromises from 'fs/promises';
+// import path from 'path';
 import Content from './content';
 import { Recipe } from './types';
+import postgres from 'postgres';
+
+const sql = postgres(process.env.POSTGRES_URL!, {
+	database: 'recipes',
+	user: 'recipeuser',
+	password: 'cookiesaretasty9876'
+});
 
 async function
 RecipePage(
 { searchParams }:
 	{ searchParams: Promise<{ [key: string]: string | string[] | undefined }> }
 ) {
-	const recipeId = (await searchParams).recipeId;
-	const filePath = path.join(process.cwd(), "public/recipe-data/" + recipeId + ".json");
-	const jsonData = await fsPromises.readFile(filePath, 'utf8');
-	const recipe: Recipe = JSON.parse(jsonData);
+	const tmp: string | string[] | undefined = (await searchParams).recipeId;
+	let recipeId: string;
+	if (Array.isArray(tmp)) recipeId = tmp[0];
+	else recipeId = tmp ?? "";
+	// const recipeId: string = (await searchParams).recipeId;
+	// const filePath = path.join(process.cwd(), "public/recipe-data/" + recipeId + ".json");
+	// const jsonData = await fsPromises.readFile(filePath, 'utf8');
+	// const jsonData = await sql`
+	// SELECT json FROM recipes WHERE id = ${recipeId}
+	// `
+	const jsonData = await sql<[{json: Recipe}]>`
+	SELECT json
+	FROM recipes
+	WHERE id = ${ recipeId }`;
 
+	const recipe = jsonData[0].json;
+	
 	return (
 		<Content recipe={recipe} />
 	)
